b30348198bfd074cb614a49c242eca7a8aca1e29
Alejandro Salas <asalas.wd@outlook.com>
2022-12-09 19:25:55 -0500

    create components folder and update Info and App files

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Info from "./components/Info";
+
+const App = () => {
+  return(
+    <div>
+      <Info />
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/Info.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Info = () => {
+  return(
+    <h1>Alejandro Salas</h1>
+  )
+}
+
+export default Info;
