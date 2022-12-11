959e054dcb6fece94e6583cd48e22928f4e609e1
Alejandro Salas <asalas.wd@outlook.com>
2022-12-11 00:33:44 -0500

    update Email link

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Buttons = () => {
   return(
     <div>      
-      <a className="email-btn"><i className="fa fa-envelope"></i>Email</a>
+      <a className="email-btn" href="mailto:asalasv.dev@gmail.com"><i className="fa fa-envelope"></i>Email</a>
       <a className="linkedin-btn"><i className="fa fa-linkedin"></i>LinkedIn</a>
     </div>
   )
