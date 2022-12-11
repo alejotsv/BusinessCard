d196661190c51169e12ab51bd8d355f65923030a
Alejandro Salas <asalas.wd@outlook.com>
2022-12-11 00:42:51 -0500

    update LinkedIn button

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -4,7 +4,7 @@ const Buttons = () => {
   return(
     <div>      
       <a className="email-btn" href="mailto:asalasv.dev@gmail.com"><i className="fa fa-envelope"></i>Email</a>
-      <a className="linkedin-btn"><i className="fa fa-linkedin"></i>LinkedIn</a>
+      <a className="linkedin-btn" href="https://www.linkedin.com/in/alejotsv/" target="_blank"><i className="fa fa-linkedin"></i>LinkedIn</a>
     </div>
   )
 }
