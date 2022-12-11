d0062c4469c14e6235f3b565ea66b7286ce7f0d7
Alejandro Salas <asalas.wd@outlook.com>
2022-12-10 19:09:14 -0500

    add rectangle with background color

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -3,11 +3,8 @@ import profile_pic from "../assets/profile.jpg";
 
 const Info = () => {
   return(
-    <div className="info">
-      <div className="img-box">
-        <img src={profile_pic} alt="profile-pic" height="317" />
-      </div>
-      <h1>Alejandro Salas</h1>
+    <div className="info-container">
+      
     </div>
   )
 }
