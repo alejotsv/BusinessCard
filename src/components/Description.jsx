6bd904ab0c93ebd7b1916882db78a18e6317072a
Alejandro Salas <asalas.wd@outlook.com>
2022-12-10 23:15:30 -0500

    complete description section

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import About from "./About";
+import Interests from "./Interests";
 
 const Description = () => {
   return(
     <div className="description">
       <About />
+      <Interests />
     </div>
   )
 }
---
+++ src/components/Interests.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Interests = () => {
+  return(
+    <div className="inner-description">
+      <h2>Interests</h2>
+      <p>
+        I am passionate about soccer. I am a coffee enthusiast. I love the 90s, cinema and all things Marvel.
+      </p>
+    </div>
+  )
+}
+
+export default Interests;
