74e9d690667646ed7957fc8d3119232b5a27a613
Alejandro Salas <asalas.wd@outlook.com>
2022-12-10 20:55:24 -0500

    add link to website

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -5,6 +5,7 @@ const Title = () => {
     <div className="title">
       <h1>Alejandro Salas</h1>
       <h2>Full Stack Developer</h2>
+      <h3><a href="https//www.asalas.dev" target="_blank">asalas.dev</a></h3>
     </div>
   )
 }
