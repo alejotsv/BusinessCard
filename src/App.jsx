b8e5485e569d0e3e1170d496667e30810108fe30
Alejandro Salas <asalas.wd@outlook.com>
2022-12-10 22:47:29 -0500

    add Description card

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import Image from "./components/Image";
 import Info from "./components/Info";
 import Buttons from "./components/Buttons";
+import About from "./components/About";
+import Description from "./components/Description";
 
 const App = () => {
   return(
     <div className="card-container">      
         <Image />
         <Info />
-        <Buttons />    
+        <Buttons />
+        <Description />        
     </div>
   )
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const About = () => {
+  return(
+    <div className="About">
+      <h2>About</h2>
+      <p>I like making user experiences that seem effortless. I keep this in mind when creating web applications using ReactJs, back-end projects with Java, or implementing automation testing in Python and JavaScript.</p>
+    </div>
+  );
+}
+
+export default About;
